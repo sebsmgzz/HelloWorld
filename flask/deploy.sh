0c2dc4101facc6abcada09de5c0d58c943a891ff
sebsmgzz <sebastian.mgzz@outlook.com>
2021-02-13 16:58:12 -0600

    feat: flask app

---
+++ flask/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Variables definitions
+IMAGE_NAME = "flask_app"
+REPO_PATH = .
+LOCAL_PORT = 80
+APP_PORT = 5000
+
+# Build docker image
+docker build -t $IMAGE_NAME $REPO_PATH
+
+# Run docker container
+docker run -p $LOCAL_PORT:$APP_PORT $IMAGE_NAME
